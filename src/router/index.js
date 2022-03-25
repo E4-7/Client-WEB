ea323e2ead5cf1a3dc66c0967efa74b8e2cd8ea6
na0 <koonayoung51@naver.com>
2022-03-26 01:34:20 +0900

    INIT

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,17 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import Base from '@/views/Layout/Base.vue';
+import Main from '@/views/Layout/MainPage.vue';
 
 Vue.use(VueRouter);
 
 const routes = [
   {
-    path: '',
+    path: '/',
+    component: Main,
+  },
+  {
+    path: '/base',
     component: Base,
     meta: { requiresAuth: true },
     children: [
---
+++ src/views/Layout/MainPage.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-app>
+    <v-main>
+      <v-card elevation="2">
+        <h1>
+          page.
+        </h1>
+      </v-card>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'Main',
+  components: {},
+};
+</script>
