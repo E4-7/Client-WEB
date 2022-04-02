953974e1f8537dd32b3ecef4332c4aa7917f7f5c
na0 <koonayoung51@naver.com>
2022-04-02 13:02:29 +0900

    FIX : 뷰티파이 적용 오류 수정

--- src/views/Layout/Student/VerifyIdentity.vue
+++ src/views/Layout/Student/VerifyIdentity.vue
@@ -1,115 +1,119 @@
 <template>
-  <v-main class="main-page-cardview-layout">
-    <v-container fill-height fluid>
-      <v-row align-center justify="center">
-        <v-stepper
-          v-model="e1"
-          class="main-page-cardview"
-          elevation="2"
-          outlined
-          min-width="1200"
-          min-height="700"
-        >
-          <v-stepper-header>
-            <v-stepper-step :complete="e1 > 1" step="1">
-              입장코드
-            </v-stepper-step>
-            <v-divider></v-divider>
-            <v-stepper-step :complete="e1 > 2" step="2">
-              학번, 이름
-            </v-stepper-step>
-            <v-divider></v-divider>
-            <v-stepper-step step="3">
-              본인인증(학생증)
-            </v-stepper-step>
-          </v-stepper-header>
+  <v-app>
+    <v-main class="main-page-cardview-layout">
+      <v-container fill-height fluid>
+        <v-row align-center justify="center">
+          <v-stepper
+            v-model="e1"
+            class="main-page-cardview"
+            elevation="2"
+            outlined
+            min-width="1200"
+            min-height="700"
+          >
+            <v-stepper-header>
+              <v-stepper-step :complete="e1 > 1" step="1">
+                입장코드
+              </v-stepper-step>
+              <v-divider></v-divider>
+              <v-stepper-step :complete="e1 > 2" step="2">
+                학번, 이름
+              </v-stepper-step>
+              <v-divider></v-divider>
+              <v-stepper-step step="3">
+                본인인증(학생증)
+              </v-stepper-step>
+            </v-stepper-header>
 
-          <v-stepper-items>
-            <v-stepper-content step="1">
-              <v-card class="mb-12" color="grey lighten-1" height="500px">
-                <v-row class="main-page-text">
-                  <v-card-text>
-                    <div style="font-size: 30px">
-                      입장 코드
-                    </div>
-                    <br />
-                    <p class="text-h2 bold text--primary">
-                      전달 받은 입장 코드를 입력해주세요!
-                    </p>
-                    <br />
-                    <br />
-                    <v-text-field
-                      placeholder="input code"
-                      outlined
-                      label="코드   "
-                      :rules="rules"
-                    >
-                    </v-text-field>
-                  </v-card-text>
-                </v-row>
-              </v-card>
+            <v-stepper-items>
+              <v-stepper-content step="1">
+                <v-card class="mb-12" color="grey lighten-1" height="500px">
+                  <v-row class="main-page-text">
+                    <v-card-text>
+                      <br />
+                      <br />
+                      <div style="font-size: 30px">
+                        입장 코드
+                      </div>
+                      <br />
+                      <p class="body-1 my-4 bold text--primary">
+                        전달 받은 입장 코드를 입력해주세요!
+                      </p>
+                      <br />
+                      <br />
+                      <v-text-field
+                        placeholder="input code"
+                        outlined
+                        label="코드   "
+                        :rules="rules"
+                      >
+                      </v-text-field>
+                    </v-card-text>
+                  </v-row>
+                </v-card>
 
-              <div class="verify-identity-button">
-                <v-btn rounded color="primary" dark @click="e1 = 2">
-                  다음
+                <div class="verify-identity-button">
+                  <v-btn rounded color="primary" dark @click="e1 = 2">
+                    다음
+                  </v-btn>
+                </div>
+              </v-stepper-content>
+
+              <v-stepper-content step="2">
+                <v-card class="mb-12" color="grey lighten-1" height="500px">
+                  <v-row class="main-page-text">
+                    <v-card-text>
+                      <div style="font-size: 30px">
+                        학번
+                      </div>
+                      <br />
+                      <v-text-field outlined label="학번" :rules="rules">
+                      </v-text-field>
+                      <div style="font-size: 30px">
+                        이름
+                      </div>
+                      <br />
+                      <v-text-field
+                        placeholder="input your name"
+                        outlined
+                        label="이름"
+                        :rules="rules"
+                      >
+                      </v-text-field>
+                    </v-card-text>
+                  </v-row>
+                </v-card>
+                <div class="verify-identity-button">
+                  <v-btn text @click="e1 = 1">
+                    뒤로
+                  </v-btn>
+                  <v-btn rounded color="primary" dark @click="e1 = 3">
+                    다음
+                  </v-btn>
+                </div>
+              </v-stepper-content>
+
+              <v-stepper-content step="3">
+                <v-card
+                  class="mb-12"
+                  color="grey lighten-1"
+                  height="500px"
+                ></v-card>
+                <v-btn
+                  class="verify-identity-button"
+                  rounded
+                  color="primary"
+                  @click="e1 = 1"
+                >
+                  완료
                 </v-btn>
-              </div>
-            </v-stepper-content>
-
-            <v-stepper-content step="2">
-              <v-card class="mb-12" color="grey lighten-1" height="500px">
-                <v-row class="main-page-text">
-                  <v-card-text>
-                    <div style="font-size: 30px">
-                      학번
-                    </div>
-                    <br />
-                    <v-text-field outlined label="학번" :rules="rules">
-                    </v-text-field>
-                    <div style="font-size: 30px">
-                      이름
-                    </div>
-                    <br />
-                    <v-text-field
-                      placeholder="input your name"
-                      outlined
-                      label="이름"
-                      :rules="rules"
-                    >
-                    </v-text-field>
-                  </v-card-text>
-                </v-row>
-              </v-card>
-              <div class="verify-identity-button">
-                <v-btn text @click="e1 = 1">
-                  뒤로
-                </v-btn>
-                <v-btn rounded color="primary" dark @click="e1 = 3">
-                  다음
-                </v-btn>
-              </div>
-            </v-stepper-content>
-
-            <v-stepper-content step="3">
-              <v-card
-                class="mb-12"
-                color="grey lighten-1"
-                height="500px"
-              ></v-card>
-              <v-btn
-                class="verify-identity-button"
-                rounded
-                color="primary"
-                @click="e1 = 1"
-              >
-                완료
-              </v-btn>
-            </v-stepper-content>
-          </v-stepper-items>
-        </v-stepper>
-      </v-row>
-    </v-container>
-  </v-main>
+              </v-stepper-content>
+            </v-stepper-items>
+          </v-stepper>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
