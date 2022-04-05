a2e32e5a28975d36df0f9f3d7a001c86a8180510
unknown <koonayoung51@naver.com>
2022-04-05 23:53:30 +0900

    fix : menubar image 경로 문제

--- src/views/Layout/MenuBar.vue
+++ src/views/Layout/MenuBar.vue
@@ -2,11 +2,17 @@
   <div>
     <nav>
       <v-toolbar class="primary" dark height="65">
+        <v-img
+          class="mx-2"
+          src="src/assets/images/e47_logo_blue.png"
+          max-height="100"
+          max-width="70"
+          contain
+        ></v-img>
         <v-toolbar-title class="text-uppercase gray--text">
           <span class="font-weight-light">E4/</span>
           <span>7</span>
         </v-toolbar-title>
-
         <v-spacer></v-spacer>
         <v-btn text color="black">
           <span>Log Out</span>
