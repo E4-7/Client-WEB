fd828707c81efd17a0f618586092453bd9a41481
unknown <koonayoung51@naver.com>
2022-05-02 17:46:29 +0900

    set : userStore 나누기

---
+++ src/store/modules/userStore.js
@@ -0,0 +1,19 @@
+// import router from '@/router';
+
+// const userStore = {
+//   state: { userId: '', userName: '',
+//   mutations: {
+//     login: function(state, payload) {
+//       (state.userId = payload.userId), (state.userName = payload.userName), (state.token = payload.token);
+//     },
+//     loginCheck: function(state) {
+//       if (!state.token) {
+//         router.push({ name: 'login' }).catch(error => {
+//           console.log(error);
+//         });
+//       }
+//     },
+//   },
+// };
+
+// export default userStore,
